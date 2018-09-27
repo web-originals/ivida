94eb004f294e18df604fa54bf7ebb3c1cf58a609
ikor <igor_korenovsk@mail.ru>
2018-09-27 13:33:10 +0300

    тест функции

--- single-portfolio.php
+++ single-portfolio.php
@@ -58,41 +58,34 @@ function get_content_portfolio()
             }
         </style>
     <script>
-        if(window.jQuery) {
+        function($) {
             $(document).ready(function () {
-                galery();
+                //клик по ссылкам верхнего уровня
+                $('.slideshow_pic').on('click', function (e) {
+                    // запрещает дефолдное действие эл. переход по ссылке
+                    e.preventDefault();
+                    // сохраняем переменные
+                    var $this = $(this),
+                        //сохраняем li по которой кликнули
+                        item = $this.closest('.slideshow_item'),
+                        //сохраняем контент со слайдером
+                        container = $this.closest('.slideshow'),
+                        //сохраняем блок отображения вида
+                        display = container.find('.slideshow_display'),
+                        //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
+                        paht = item.find('img').attr('src'),
+                        // время анимации
+                        duration = 300;
+                    if (!item.hasClass('active')) {
+                        //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
+                        item.addClass('active').siblings().removeClass('active');
+                        display.find('img').fadeOut(duration, function () {
+                            $(this).attr('src', paht).fadeIn(duration);
+                        })
+                    }
+                });
             });
-        }else {
-            document.addEventListener("DOMContentLoaded", function (event) {
-                galery();
-            });
-        }
-        function galery() {
-            //клик по ссылкам верхнего уровня
-            $('.slideshow_pic').on('click', function (e) {
-                // запрещает дефолдное действие эл. переход по ссылке
-                e.preventDefault();
-                // сохраняем переменные
-                var $this = $(this),
-                    //сохраняем li по которой кликнули
-                    item = $this.closest('.slideshow_item'),
-                    //сохраняем контент со слайдером
-                    container = $this.closest('.slideshow'),
-                    //сохраняем блок отображения вида
-                    display = container.find('.slideshow_display'),
-                    //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
-                    paht = item.find('img').attr('src'),
-                    // время анимации
-                    duration = 300;
-                if (!item.hasClass('active')) {
-                    //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
-                    item.addClass('active').siblings().removeClass('active');
-                    display.find('img').fadeOut(duration, function () {
-                        $(this).attr('src', paht).fadeIn(duration);
-                    })
-                }
-            });
-        }
+        }(jQuery);
     </script>
 <?php
     global $post;
