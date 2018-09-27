ff8ec1c771b88a65a853d684969b9b898ac141f9
ikor <igor_korenovsk@mail.ru>
2018-09-27 14:25:19 +0300

    сделал функцию галереи  футере

--- functions.php
+++ functions.php
@@ -6,6 +6,46 @@
  * Time: 21:42
  */
 
+function footer_script()
+{
+    ?>
+    <script>
+
+
+                function galery_run() {
+                    //клик по ссылкам верхнего уровня
+                    $('.slideshow_pic').on('click', function (e) {
+                        // запрещает дефолдное действие эл. переход по ссылке
+                        e.preventDefault();
+                        // сохраняем переменные
+                        var $this = $(this),
+                            //сохраняем li по которой кликнули
+                            item = $this.closest('.slideshow_item'),
+                            //сохраняем контент со слайдером
+                            container = $this.closest('.slideshow'),
+                            //сохраняем блок отображения вида
+                            display = container.find('.slideshow_display'),
+                            //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
+                            paht = item.find('img').attr('src'),
+                            // время анимации
+                            duration = 300;
+                        if (!item.hasClass('active')) {
+                            //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
+                            item.addClass('active').siblings().removeClass('active');
+                            display.find('img').fadeOut(duration, function () {
+                                $(this).attr('src', paht).fadeIn(duration);
+                            })
+                        }
+                    });
+                }
+
+
+
+    </script>
+    <?php
+}
+add_action('wp_footer', 'footer_script',100);
+
 // стили просто так не подключаются на странице поиска
 function header_theme(){
     if(is_search() || is_singular( 'portfolio' ) ){
