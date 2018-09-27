14a8f4374a2f1e975a6b92e8e324dd725a06258d
ikor <igor_korenovsk@mail.ru>
2018-09-27 13:21:54 +0300

    сделал проверку на jquery для галереи

--- single-portfolio.php
+++ single-portfolio.php
@@ -58,46 +58,41 @@ function get_content_portfolio()
             }
         </style>
     <script>
-        <?php if(!( isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')){
-            ?>document.addEventListener("DOMContentLoaded", function(event) {
-       <?php
-                $temp = true;
-        } else{
-            $temp = false;
-        }?>
+        if(window.jQuery) {
             $(document).ready(function () {
-                //клик по ссылкам верхнего уровня
-                $('.slideshow_pic').on('click', function (e) {
-                    // запрещает дефолдное действие эл. переход по ссылке
-                    e.preventDefault();
-                    // сохраняем переменные
-                    var $this = $(this),
-                        //сохраняем li по которой кликнули
-                        item = $this.closest('.slideshow_item'),
-                        //сохраняем контент со слайдером
-                        container = $this.closest('.slideshow'),
-                        //сохраняем блок отображения вида
-                        display = container.find('.slideshow_display'),
-                        //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
-                        paht = item.find('img').attr('src'),
-                        // время анимации
-                        duration = 300;
-                    if (!item.hasClass('active')) {
-                        //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
-                        item.addClass('active').siblings().removeClass('active');
-                        display.find('img').fadeOut(duration, function () {
-                            $(this).attr('src', paht).fadeIn(duration);
-                        })
-                    }
-                });
+                galery();
             });
-       <?php
-if($temp){
-        ?>
+        }else {
+            document.addEventListener("DOMContentLoaded", function (event) {
+                galery();
             });
-        <?php
-}
-        ?>
+        }
+        function galery() {
+            //клик по ссылкам верхнего уровня
+            $('.slideshow_pic').on('click', function (e) {
+                // запрещает дефолдное действие эл. переход по ссылке
+                e.preventDefault();
+                // сохраняем переменные
+                var $this = $(this),
+                    //сохраняем li по которой кликнули
+                    item = $this.closest('.slideshow_item'),
+                    //сохраняем контент со слайдером
+                    container = $this.closest('.slideshow'),
+                    //сохраняем блок отображения вида
+                    display = container.find('.slideshow_display'),
+                    //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
+                    paht = item.find('img').attr('src'),
+                    // время анимации
+                    duration = 300;
+                if (!item.hasClass('active')) {
+                    //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
+                    item.addClass('active').siblings().removeClass('active');
+                    display.find('img').fadeOut(duration, function () {
+                        $(this).attr('src', paht).fadeIn(duration);
+                    })
+                }
+            });
+        }
     </script>
 <?php
     global $post;
