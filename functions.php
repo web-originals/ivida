841ae38c06acc06581f55d1594d6e2e61cb8a7e8
ikor <igor_korenovsk@mail.ru>
2018-09-27 14:52:38 +0300

    измениел функцию галереи

--- functions.php
+++ functions.php
@@ -11,33 +11,34 @@ function footer_script()
     ?>
     <script>
 
-
-                function galery_run() {
-                    //клик по ссылкам верхнего уровня
-                    $('.slideshow_pic').on('click', function (e) {
-                        // запрещает дефолдное действие эл. переход по ссылке
-                        e.preventDefault();
-                        // сохраняем переменные
-                        var $this = $(this),
-                            //сохраняем li по которой кликнули
-                            item = $this.closest('.slideshow_item'),
-                            //сохраняем контент со слайдером
-                            container = $this.closest('.slideshow'),
-                            //сохраняем блок отображения вида
-                            display = container.find('.slideshow_display'),
-                            //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
-                            paht = item.find('img').attr('src'),
-                            // время анимации
-                            duration = 300;
-                        if (!item.hasClass('active')) {
-                            //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
-                            item.addClass('active').siblings().removeClass('active');
-                            display.find('img').fadeOut(duration, function () {
-                                $(this).attr('src', paht).fadeIn(duration);
-                            })
-                        }
-                    });
-                }
+        document.addEventListener("DOMContentLoaded", function(event) {
+            function galery_run() {
+                //клик по ссылкам верхнего уровня
+                $('.slideshow_pic').on('click', function (e) {
+                    // запрещает дефолдное действие эл. переход по ссылке
+                    e.preventDefault();
+                    // сохраняем переменные
+                    var $this = $(this),
+                        //сохраняем li по которой кликнули
+                        item = $this.closest('.slideshow_item'),
+                        //сохраняем контент со слайдером
+                        container = $this.closest('.slideshow'),
+                        //сохраняем блок отображения вида
+                        display = container.find('.slideshow_display'),
+                        //принцип работы слайдера берем src у img  и вставляем его в блок slideshow_display
+                        paht = item.find('img').attr('src'),
+                        // время анимации
+                        duration = 300;
+                    if (!item.hasClass('active')) {
+                        //добавили клас у актива, чтоб не нажимался анимация на одином и тодже слайде, а у остальных его убираем
+                        item.addClass('active').siblings().removeClass('active');
+                        display.find('img').fadeOut(duration, function () {
+                            $(this).attr('src', paht).fadeIn(duration);
+                        })
+                    }
+                });
+            }
+        }
 
 
 
