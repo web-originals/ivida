13e3af27cd17ed98f843c1efc6051e97018c39c0
ikor <igor_korenovsk@mail.ru>
2018-11-18 19:31:10 +0300

    fix simgle-portfolio fields

--- single-portfolio.php
+++ single-portfolio.php
@@ -106,11 +106,19 @@ function get_content_portfolio($ajax_work)
             [/vc_column][vc_column width="1/3"][vc_column_text]<div class="project-info">
                     <h2>' . get_the_title() . '</h2>
                     <a href="' . get_home_url() . '/order/?add=' . $post->ID . '" class ="btn btn-more btn-big" target="_self" >Обратная связь</a>
-                    <div class="details">
-                        <div class="info-text"><span class="title">Цена</span><span class="val">' . get_field("price") . '</span></div>
-                        <div class="info-text"><span class="title">Размер</span><span class="val">' . get_field("size") . '</span></div>
-                        <div class="info-text"><span class="title">Вес</span><span class="val">' . get_field("weight") . '</span></div>
-                    </div>' . get_the_content() . '
+                    ';
+    if(!empty(get_field("price")) || !empty(get_field("size")) || !empty(get_field("weight"))){
+        $content.= '<div class="details">';
+        if(!empty(get_field("price")))
+            $content.= '<div class="info-text"><span class="title">Цена</span><span class="val">' . get_field("price") . '</span></div>';
+        if(!empty(get_field("size")))
+            $content.= '<div class="info-text"><span class="title">Размер</span><span class="val">' . get_field("size") . '</span></div>';
+        if(!empty(get_field("weight")))
+            $content.= '<div class="info-text"><span class="title">Вес</span><span class="val">' . get_field("weight") . '</span></div>';
+
+        $content.=   '</div>';
+    }
+    $content.= get_the_content() . '
                 </div>[/vc_column_text][/vc_column][/vc_row]';
     return apply_filters('the_content', $content);
 }
@@ -122,13 +130,13 @@ function get_content_portfolio($ajax_work)
             echo get_content_portfolio($ajax_work);
         ?>
 		<?php endwhile; ?>
-	</div>		
+	</div>
 <?php }else { ?>
 	<?php get_header(); ?>
 		<?php global $archi_option; ?>
 		<?php if($archi_option['subpage-switch']!=false){ ?>
 			<section id="subheader" data-speed="8" data-type="background" class="padding-top-bottom"
-				<?php if( function_exists( 'rwmb_meta' ) ) { ?>       
+				<?php if( function_exists( 'rwmb_meta' ) ) { ?>
 			        <?php $images = rwmb_meta( '_cmb_portfolio_subheader', "type=image" ); ?>
 			        <?php if($images){ foreach ( $images as $image ) { ?>
 			        <?php $img =  $image['full_url']; ?>
@@ -138,13 +146,13 @@ function get_content_portfolio($ajax_work)
 			>
 			    <div class="container">
 			        <div class="row">
-			            <div class="col-md-12">                
+			            <div class="col-md-12">
 			                <h1><?php the_title(); ?></h1>
-			                <?php 
-		                        if(function_exists('archi_breadcrumbs')): 
+			                <?php
+		                        if(function_exists('archi_breadcrumbs')):
 		                            archi_breadcrumbs();
 		                        endif;
-		                    ?> 
+		                    ?>
 			            </div>
 			        </div>
 			    </div>
@@ -154,44 +162,44 @@ function get_content_portfolio($ajax_work)
 		<?php }else{ ?>
 		    <section class="no-subpage"></section>
 		<?php } ?>
-		
+
 		<?php if(isset($archi_option['single_navigation']) and $archi_option['single_navigation']=="navontop" ){ ?>
 			<div id="portfolio-controls">
-		        <div class="left-right-portfolio"> 
+		        <div class="left-right-portfolio">
 		            <div class="portfolio-icon">
-		            	<?php 
-			            	if ($archi_option['portfolio_navigation'] == 'samecategory') { 
+		            	<?php
+			            	if ($archi_option['portfolio_navigation'] == 'samecategory') {
 								previous_post_link('%link', __('<i class="fa fa-angle-double-left"></i>', 'archi'), TRUE, ' ', 'categories');
-			            	}else{	
+			            	}else{
 			            		previous_post_link('%link', __('<i class="fa fa-angle-double-left"></i>', 'archi'), $post->max_num_pages );
-			            	} 
+			            	}
 		            	?>
 		            </div>
 		        </div>
 		        <a href="<?php echo esc_url($archi_option['portfolio_link']); ?>">
-		            <div class="center-portfolio"> 
+		            <div class="center-portfolio">
 		                <div class="portfolio-icon fa-th"></div>
-		            </div> 
+		            </div>
 		        </a>
 		        <div class="left-right-portfolio">
 		            <div class="portfolio-icon">
-		            	<?php 
-			            	if ($archi_option['portfolio_navigation'] == 'samecategory') { 
+		            	<?php
+			            	if ($archi_option['portfolio_navigation'] == 'samecategory') {
 								next_post_link('%link', __('<i class="fa fa-angle-double-right"></i>', 'archi'), TRUE, ' ', 'categories');
-			            	}else{	
+			            	}else{
 			            		next_post_link('%link', __('<i class="fa fa-angle-double-right"></i>', 'archi'), $post->max_num_pages );
-			            	} 
+			            	}
 		            	?>
-		            </div> 
+		            </div>
 		        </div>
 		    </div>
-		<?php } ?>    
+		<?php } ?>
 
 		<div id="content">
 			<?php if ( have_posts() ) : ?>
 				<?php while (have_posts()) : the_post(); ?>
 					<?php echo get_content_portfolio($ajax_work); ?>
-				<?php endwhile; ?>			
+				<?php endwhile; ?>
 			<?php endif; ?>
 			<section class="single-portfolio-bottom">
 				<div class="container">
@@ -200,17 +208,17 @@ function get_content_portfolio($ajax_work)
 							<?php if ($archi_option['project_sharing']!=false) { ?>
 								<div class="socials-portfolio socials-rounded">
 									<h4>Поделиться:</h4>
-									<div class="socials-sharing"> 
-										<a class="socials-item" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" title="Facebook"><i class="fa fa-facebook"></i></a> 
-										<a class="socials-item" target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php the_permalink(); ?>" title="Twitter"><i class="fa fa-twitter"></i></a> 
-										<a class="socials-item" target="_blank" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" title="Google Plus"><i class="fa fa-google-plus"></i></a> 
-										<a class="socials-item" target="_blank" href="https://www.pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&description=<?php the_title(); ?>" title="Pinterest"><i class="fa fa-pinterest"></i></a> 																				
+									<div class="socials-sharing">
+										<a class="socials-item" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" title="Facebook"><i class="fa fa-facebook"></i></a>
+										<a class="socials-item" target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php the_permalink(); ?>" title="Twitter"><i class="fa fa-twitter"></i></a>
+										<a class="socials-item" target="_blank" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" title="Google Plus"><i class="fa fa-google-plus"></i></a>
+										<a class="socials-item" target="_blank" href="https://www.pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&description=<?php the_title(); ?>" title="Pinterest"><i class="fa fa-pinterest"></i></a>
 										<a class="socials-item" href="http://digg.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" target="_blank"><i class="fa fa-digg" aria-hidden="true"></i></a>
-										<a class="socials-item" target="_blank" href="http://www.tumblr.com/share/link?url=<?php the_permalink(); ?>&name=<?php the_title(); ?>&description=<?php echo get_the_excerpt(); ?>" title="Tumblr"><i class="fa fa-tumblr"></i></a> 
+										<a class="socials-item" target="_blank" href="http://www.tumblr.com/share/link?url=<?php the_permalink(); ?>&name=<?php the_title(); ?>&description=<?php echo get_the_excerpt(); ?>" title="Tumblr"><i class="fa fa-tumblr"></i></a>
 										<a class="socials-item" href="http://reddit.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" target="_blank"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a>
-										<a class="socials-item" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&summary=<?php echo get_the_excerpt(); ?>" title="LinkedIn"><i class="fa fa-linkedin"></i></a> 
-										<a class="socials-item" target="_blank" href="http://www.stumbleupon.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" title="StumbleUpon"><i class="fa fa-stumbleupon"></i></a>									
-										<a class="socials-item" href="https://delicious.com/save?v=5&provider=<?php bloginfo( 'name' ); ?>&noui&jump=close&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" target="_blank"><i class="fa fa-delicious" aria-hidden="true"></i></a>										
+										<a class="socials-item" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&summary=<?php echo get_the_excerpt(); ?>" title="LinkedIn"><i class="fa fa-linkedin"></i></a>
+										<a class="socials-item" target="_blank" href="http://www.stumbleupon.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" title="StumbleUpon"><i class="fa fa-stumbleupon"></i></a>
+										<a class="socials-item" href="https://delicious.com/save?v=5&provider=<?php bloginfo( 'name' ); ?>&noui&jump=close&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>" target="_blank"><i class="fa fa-delicious" aria-hidden="true"></i></a>
 										<a class="socials-item" href="http://vk.com/share.php?url=<?php the_permalink(); ?>" target="_blank"><i class="fa fa-vk"></i></a>
 									</div>
 								</div>
@@ -220,21 +228,21 @@ function get_content_portfolio($ajax_work)
 								<div class="double-divider clearfix"></div>
 								<div class="portfolio-navigation clearfix">
 									<div class="portfolio-btn-prev">
-										<?php 
-							            	if ($archi_option['portfolio_navigation'] == 'samecategory') { 
+										<?php
+							            	if ($archi_option['portfolio_navigation'] == 'samecategory') {
 												previous_post_link('%link', __('<i class="fa fa-chevron-left"></i> Prev', 'archi'), TRUE, ' ', 'categories');
-							            	}else{	
+							            	}else{
 							            		previous_post_link('%link', __('<i class="fa fa-chevron-left"></i> Prev', 'archi'), $post->max_num_pages );
-							            	} 
+							            	}
 						            	?>
 									</div>
 									<div class="portfolio-btn-next">
-										<?php 
-							            	if ($archi_option['portfolio_navigation'] == 'samecategory') { 
+										<?php
+							            	if ($archi_option['portfolio_navigation'] == 'samecategory') {
 												next_post_link('%link', __('Next <i class="fa fa-chevron-right"></i>', 'archi'), TRUE, ' ', 'categories');
-							            	}else{	
+							            	}else{
 							            		next_post_link('%link', __('Next <i class="fa fa-chevron-right"></i>', 'archi'), $post->max_num_pages );
-							            	} 
+							            	}
 						            	?>
 									</div>
 								</div>
@@ -244,5 +252,5 @@ function get_content_portfolio($ajax_work)
 				</div>
 			</section>
 		</div>
-	<?php get_footer(); ?>	
+	<?php get_footer(); ?>
 <?php } ?>
