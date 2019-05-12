d14f4722e944ebe34bfe33ac938af96ac16bebd5
ikor <igor_korenovsk@mail.ru>
2019-05-12 20:21:15 +0300

    rename title testimonials

--- single-testimonial.php
+++ single-testimonial.php
@@ -43,7 +43,7 @@
                     <?php if (have_posts()){ ?>
                         <div id="post-<?php the_ID(); ?>" <?php post_class('page-content'); ?>>
                             <?php while (have_posts()) : the_post()?>
-                                <h2><?php the_title(); ?></h2>
+                                <h2>Отзывы</h2>
                                 <?php the_post_thumbnail() ?>
                                 <?php the_content(); ?>
                                 <?php
